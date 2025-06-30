e96518d2e670313949778db262428435c6f6f0c6
jon-H34400 <ge38yub@mytum.de>
2025-06-30 14:40:29 +0200

    Remove build folder from repo and add it to .gitignore

--- build/CMakeFiles/3DBodyAnimation.dir/src/smpl_model.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/3DBodyAnimation.dir/src/smpl_model.cpp.o: \
- /home/jonathan-herwerth/git/3DBodyAnimation/src/smpl_model.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/3DBodyAnimation.dir/src/visualizer.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/3DBodyAnimation.dir/src/visualizer.cpp.o: \
- /home/jonathan-herwerth/git/3DBodyAnimation/src/visualizer.cpp \
- /usr/include/stdc-predef.h
